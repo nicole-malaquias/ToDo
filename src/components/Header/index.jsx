19c7183ea2e1e3093f0abde85d666513ecf798bb
Nicole <nicolemalaquias91@gmail.com>
2021-09-16 14:38:42 -0300

    trocando os icones

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,8 @@
 import * as S from "./styled"
 import { useHistory } from "react-router-dom";
-import {CgArrowRightR , CgArrowLeftR} from "react-icons/cg"
+
+import { IoIosJournal } from "react-icons/io";
+import { IoMdHome } from "react-icons/io";
 
 const Header = () => {
   
@@ -14,12 +16,12 @@ const Header = () => {
     <S.Container>
       <S.Central>
         <div className="sendTo" onClick={() => sendTo("/")} >
-          <h2>  <CgArrowLeftR size={30} color="#3FAD27" /> Home</h2>
+          <h2>  <IoMdHome size={30} color="#ffffff" /> Home</h2>
               
         </div>
 
         <div className="sendTo" onClick={() => sendTo("/finish")} >
-          <h2> <CgArrowRightR  size={30} color="#3FAD27" /> Completed </h2>
+          <h2> <IoIosJournal  size={30} color="#ffffff" /> Completed </h2>
           
         </div>
       </S.Central>
